761ed7c80295786c066cf8cb499a5e87f06fadca
=O.Mohamed <=amedhom@gmail.com>
2022-06-01 14:48:17 +0100

    fix gallery blank screen

--- lib/ui/article_details/image_full_screen.dart
+++ lib/ui/article_details/image_full_screen.dart
@@ -61,36 +61,34 @@ class _ImageFullScreenGaleryState extends State<ImageFullScreenGalery> {
     return Scaffold(
         body: Stack(
       children: [
-        Container(
-          child: PhotoViewGallery.builder(
-            scrollPhysics: const BouncingScrollPhysics(),
-            builder: (BuildContext context, int index) {
-              return PhotoViewGalleryPageOptions(
-                controller: pageController,
-                imageProvider: NetworkImage(galleryItems[index].url),
-                initialScale: PhotoViewComputedScale.contained * 1,
-                heroAttributes:
-                    PhotoViewHeroAttributes(tag: galleryItems[index].id),
-              );
-            },
+        PhotoViewGallery.builder(
+          scrollPhysics: const BouncingScrollPhysics(),
+          builder: (BuildContext context, int index) {
+            return PhotoViewGalleryPageOptions(
+              controller: pageController,
+              imageProvider: NetworkImage(galleryItems[index].url),
+              initialScale: PhotoViewComputedScale.contained * 1,
+              heroAttributes:
+                  PhotoViewHeroAttributes(tag: galleryItems[index].id),
+            );
+          },
 
-            itemCount: galleryItems.length,
+          itemCount: galleryItems.length,
 
-            loadingBuilder: (context, event) => Center(
-              child: Container(
-                width: 20.0,
-                height: 20.0,
-                child: CircularProgressIndicator(
-                  value: event == null
-                      ? 0
-                      : event.cumulativeBytesLoaded / event.expectedTotalBytes!,
-                ),
+          loadingBuilder: (context, event) => Center(
+            child: Container(
+              width: 20.0,
+              height: 20.0,
+              child: CircularProgressIndicator(
+                value: event == null
+                    ? 0
+                    : event.cumulativeBytesLoaded / event.expectedTotalBytes!,
               ),
             ),
-            // backgroundDecoration: widget.backgroundDecoration,
-
-            onPageChanged: imageChanged,
           ),
+          // backgroundDecoration: widget.backgroundDecoration,
+
+          onPageChanged: imageChanged,
         ),
         Positioned(
           bottom: 8,
@@ -101,49 +99,53 @@ class _ImageFullScreenGaleryState extends State<ImageFullScreenGalery> {
                 fontWeight: FontWeight.bold, color: Colors.white, fontSize: 16),
           ),
         ),
-        SafeArea(
-          child: Positioned(
-              top: 0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      rotateLedt();
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 40, horizontal: 10),
-                      child: Icon(
-                        FontAwesome.rotate_left,
-                        size: 28,
-                        color: AppTheme.secondaryColor(context),
+        Positioned(
+            top: 0,
+            right: 0,
+            left: 0,
+            child: SafeArea(
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        rotateLedt();
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 40, horizontal: 10),
+                        child: Icon(
+                          FontAwesome.rotate_left,
+                          size: 28,
+                          color: AppTheme.secondaryColor(context),
+                        ),
                       ),
                     ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      rotateRight();
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 40, horizontal: 10),
-                      child: Icon(
-                        FontAwesome.rotate_right,
-                        size: 28,
-                        color: AppTheme.secondaryColor(context),
+                    InkWell(
+                      onTap: () {
+                        rotateRight();
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 40, horizontal: 10),
+                        child: Icon(
+                          FontAwesome.rotate_right,
+                          size: 28,
+                          color: AppTheme.secondaryColor(context),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              )),
-        ),
-        SafeArea(
-            child: Positioned(
-          child: ReturnButton(),
+                  ],
+                ),
+              ),
+            )),
+        Positioned(
+          child: SafeArea(child: ReturnButton()),
           top: 5,
           left: 5,
-        ))
+        )
       ],
     ));
   }
